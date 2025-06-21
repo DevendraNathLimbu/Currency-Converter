d92cea208d142ca16655fcdd2fb3f039e48d2bb2
DevendraNathLimbu <srijansilimbolimbu@gmail.com>
2025-06-21 21:10:15 +0545

    api integrated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BaseURL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json';
+const BaseURL = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_lU7wkvchh4LsOlPm3sUAGa9sEGPupzJNDFRLUuS4';
 
 const dropdown = document.querySelectorAll('.dropdown select');
 
@@ -34,6 +34,7 @@ const updateFlag = (element) => {
 }
 
 const btn = document.querySelector('button');
+const final = document.querySelector('.finalValue');
 
 btn.onclick = async (e) => {
      e.preventDefault();
@@ -42,10 +43,12 @@ if(amount.value === ''  || amount.value <=0){
     amount.value = 1;
 }
 
-const url = `${BaseURL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
- let response =  await fetch(url);
- let data = await response.json();
- console.log(response);
-}
+const url = `${BaseURL}&base_currency=${fromCurrency.value}&currencies=${toCurrency.value}`;
+let response =  await fetch(url);
+let data = await response.json();
+let rate = data.data[toCurrency.value].value;
+console.log(data.data);
 
-https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@1/latest/v1/currencies/eur/jpy.json
+final.innerText = `${input.value} ${fromCurrency.value} = ${(rate.toFixed(2)*amount.value).toLocaleString()} ${toCurrency.value}`;
+
+}
